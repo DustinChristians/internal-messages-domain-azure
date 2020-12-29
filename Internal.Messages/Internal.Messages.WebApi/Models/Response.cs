539a5879bd50a7e3f3602f6ad47d52d187f1d559
Dustin <dustinchristians@gmail.com>
2020-12-29 17:54:34 -0500

    Moved the response object to the Api project.

--- Internal.Messages/Internal.Messages.WebApi/Models/Response.cs
+++ Internal.Messages/Internal.Messages.WebApi/Models/Response.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Internal.Messages.Core.Models.Wrappers
+namespace Internal.Messages.WebApi.Models
 {
     public class Response<T>
     {
+        public bool IsSuccess { get; set; }
         public T Payload { get; set; }
         public string Error { get; set; }
     }
