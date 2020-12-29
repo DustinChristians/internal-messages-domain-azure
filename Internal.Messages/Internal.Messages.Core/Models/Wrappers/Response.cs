375e463851cf7b0fec50b04627acd7403f2bb030
Dustin <dustinchristians@gmail.com>
2020-12-29 10:11:26 -0500

    Added a response model wrapper for API responses.

---
+++ Internal.Messages/Internal.Messages.Core/Models/Wrappers/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Internal.Messages.Core.Models.Wrappers
+{
+    public class Response<T>
+    {
+        public T Payload { get; set; }
+        public string Error { get; set; }
+    }
+}
